bc702cbefa4adf2e911bcf7431667606c3aee14b
ostreamBaba <1160832305@qq.com>
2018-05-08 00:40:31 +0800

    Java堆溢出测试

--- jvm/OutOfMemoryError.java
+++ jvm/OutOfMemoryError.java
@@ -8,6 +8,8 @@ import java.util.List;
  * @ Create by ostreamBaba on 18-5-7
  * @ 描述
  */
+
+//添加参数-XX:+HeapDumpOnOutOfMemoryError可以让虚拟机在出现内存溢出异常时Dump出当前的内存堆转储快照以便事后分析
 public class OutOfMemoryError {
     static class OOMObject{
     }
