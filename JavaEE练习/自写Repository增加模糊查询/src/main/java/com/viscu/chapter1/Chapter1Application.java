4a571e695d4ebb1d16d12034c280137d52e2472e
ostreamBaba <1160832305@qq.com>
2018-05-25 00:16:36 +0800

    增加模糊查询

---
+++ JavaEE练习/自写Repository增加模糊查询/src/main/java/com/viscu/chapter1/Chapter1Application.java
@@ -0,0 +1,18 @@
+package com.viscu.chapter1;
+
+import com.viscu.chapter1.dao.PersonRepository;
+import com.viscu.chapter1.support.CustomRepositoryFactoryBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories(repositoryFactoryBeanClass = CustomRepositoryFactoryBean.class)
+public class Chapter1Application {
+	@Autowired
+	PersonRepository personRepository;
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter1Application.class, args);
+	}
+}
---
+++ JavaEE练习/自写Repository增加模糊查询/src/main/java/com/viscu/chapter1/support/CustomRepositoryImpl.java
@@ -0,0 +1,29 @@
+package com.viscu.chapter1.support;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.support.JpaEntityInformation;
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+
+import javax.persistence.EntityManager;
+import java.io.Serializable;
+
+import static com.viscu.chapter1.specs.CustomerSpecs.byAuto;
+
+/**
+ * @ Create by ostreamBaba on 18-5-24
+ * @ 描述
+ */
+@NoRepositoryBean
+public class CustomRepositoryImpl<T,ID extends Serializable> extends SimpleJpaRepository<T,ID> implements CustomRepository<T,ID> {
+    private final EntityManager entityManager;
+    public CustomRepositoryImpl(Class<T> domainClass, EntityManager entityManager) {
+        super( domainClass, entityManager);
+        this.entityManager = entityManager;
+    }
+    @Override
+    public Page<T> findByAuto(T example, Pageable pageable){
+        return findAll(byAuto(entityManager,example),pageable);
+    }
+}
