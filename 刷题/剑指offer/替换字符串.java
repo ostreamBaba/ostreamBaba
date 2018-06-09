ce004dbfab356b9bb0b0a85ec4dc0da80d2219f7
ostreamBaba <1160832305@qq.com>
2018-06-09 19:51:50 +0800

    StringBuffer用法

---
+++ 刷题/剑指offer/替换字符串.java
@@ -0,0 +1,27 @@
+package com.viscu.something;
+
+/**
+ * @ Create by ostreamBaba on 18-6-9
+ * @ √Ë ˆ
+ */
+public class test {
+
+    public static String replaceSpace(StringBuffer str) {
+        String replaceString="%20";
+        StringBuilder result=new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            char ch=str.charAt(i);
+            if(str.charAt(i)==' '){
+                result.append(replaceString);
+            }else{
+                result.append(ch);
+            }
+        }
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        StringBuffer stringBuilder=new StringBuffer("We Are Happy");
+        System.out.println(replaceSpace(stringBuilder));
+    }
+}
