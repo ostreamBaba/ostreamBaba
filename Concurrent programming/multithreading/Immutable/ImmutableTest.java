176cd2bc10eb55b72b077f15d4f83855539b6227
ostreamBaba <1160832305@qq.com>
2018-06-10 21:44:46 +0800

    immutable类和加锁性能比较

---
+++ Concurrent programming/multithreading/Immutable/ImmutableTest.java
@@ -0,0 +1,43 @@
+package com.multithreading.Immutable;
+
+/**
+ * @ Create by ostreamBaba on 18-6-10
+ * @ √Ë ˆ immutable∫Õº”À¯ ≤‚ ‘ 46:14014
+ */
+
+public class ImmutableTest {
+    private static final long CALL_COUNT=100000000L;
+
+    private static void trial(String msg,long count,Object obj){
+        System.out.println(msg+" BEING");
+        long startTime=System.currentTimeMillis();
+        for (int i = 0; i < count; i++) {
+            obj.toString();
+        }
+        long endTime=System.currentTimeMillis();
+        System.out.println("msg"+" END");
+        System.out.println(endTime-startTime);
+    }
+
+    public static void main(String[] args) {
+        trial("NoSynch",CALL_COUNT,new NotSynch());
+        trial("Synch",CALL_COUNT,new Synch());
+    }
+}
+
+
+class NotSynch{
+    private final String name="NotSynch";
+    @Override
+    public String toString() {
+        return "["+name+"]";
+    }
+}
+
+class Synch{
+    private final String name="synch";
+    @Override
+    public synchronized String toString() {
+        return super.toString();
+    }
+}
