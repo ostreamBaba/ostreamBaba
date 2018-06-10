c022e93067932f7b458c86c114bb6acbaf495f40
ostreamBaba <1160832305@qq.com>
2018-06-10 21:14:19 +0800

    ArrayList并非线程安全，使用Collections.synchronizedList或者CopyOnWriteArrayList

--- Concurrent programming/multithreading/ArrayListTest.java
+++ Concurrent programming/multithreading/ArrayListTest.java
@@ -1,7 +1,10 @@
 package com.multithreading;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 
 /**
  * @ Create by ostreamBaba on 18-6-10
@@ -9,11 +12,16 @@ import java.util.List;
  */
 public class ArrayListTest {
     public static void main(String[] args) {
-        List<Integer> list=new ArrayList<Integer>();
+        //List<Integer> list=new ArrayList<Integer>();
+        //使用Collections.synchronizedList同步ArrayList实例
+        //final List<Integer> list= Collections.synchronizedList(new ArrayList<Integer>());
+        final List<Integer> list=new CopyOnWriteArrayList<Integer>();
+        //写时复制 当对集合执行写的操作时 内部已经确保安全的数组就会被整体复制 复制之后 就无需在使用迭代器以此读取元素的时候担心元素会被修改了
         new WriteThread(list).start();
         new ReadThread(list).start();
     }
 }
+
 
 class WriteThread extends Thread{
 
@@ -28,7 +36,7 @@ class WriteThread extends Thread{
     public void run() {
         for (int i = 0; true; i++) {
             list.add(i);
-            //list.remove(0);
+            list.remove(0);
         }
     }
 }
@@ -45,12 +53,22 @@ class ReadThread extends Thread{
     @Override
     public void run() {
         while (true){
-            for (int n:list){
+            /*for (int n:list){   //使用ArrayLIst出现异常
+                System.out.println(n);
+            }*/
+            /*synchronized (list){ //使用Collections.synchronizedList需要对迭代器加锁
+                for(int n:list){ //这里隐式地使用了迭代器 不加锁会出现异常(可能在读的过程中被修改)
+                    System.out.println(n);
+                }
+            }*/
+            for (int n:list){   //使用CopyOnWriteArrayList
                 System.out.println(n);
             }
         }
     }
 }
+
+
 
 /*
 class Test {
