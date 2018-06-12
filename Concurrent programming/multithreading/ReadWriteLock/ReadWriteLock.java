3ed71df78389ba6d8e005f1448876673822e9991
ostreamBaba <1160832305@qq.com>
2018-06-12 21:57:20 +0800

    实现用于读取的锁和用于写入的锁-ReadWriteLock

--- Concurrent programming/multithreading/ReadWriteLock/ReadWriteLock.java
+++ Concurrent programming/multithreading/ReadWriteLock/ReadWriteLock.java
@@ -22,14 +22,18 @@ package com.multithreading.ReadWriteLock;
     适合读取频率比写入频率高时(但写入频率比较高的话 writer角色会频繁停止对reader角色的处理 就无法体现Read-Write Lock的优点了)
  */
 
-
-
 public final class ReadWriteLock {
     private int readingReaders=0; //实际正在读取中的线程个数
     private int waitingWriters=0; //正在等待写入的线程个数
     private int writingWriters=0; //实际正在写入中的线程个数
 
     private boolean preferWriter=true;//若写入优先 则为true
+
+    /*设置preferWriter字段的原因：
+        多个线程读取不冲突
+        当时有线程进行读取操作时 那么写线程进入休眠
+        当读线程释放锁 大量(读、写)线程竞争锁 如果读线程获得锁 那么就进入休眠 如果写线程获得锁 那么进行写入操作(这样进形成了进行完读操作 优先进行写操作)
+    */
 
     //synchronized可以用于获取实例的锁(每隔实例都拥有一个锁 但是同一个锁不可以由两个以上的线程同时获取 --> 物理锁)
     //ReadWriteLock类实现了 用于读取的锁 和 用于写入的锁 两个逻辑锁(开发人员自己实现的) 但实现这两个逻辑锁的物理锁只有一个 那就是ReadWriteLock实例的锁
@@ -44,7 +48,7 @@ public final class ReadWriteLock {
 
     public synchronized void readUnlock(){  //某线程释放锁
         --readingReaders;   //正在读取的线程的数量-1
-        preferWriter=true;
+        preferWriter=true;  //read操作后优先执行write操作
         notifyAll(); //唤醒所有在等待的写 线程
     }
 
