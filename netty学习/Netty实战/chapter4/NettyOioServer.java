117425cff2be27f46d0657512138efba5f94e31d
ostreamBaba <1160832305@qq.com>
2018-07-28 11:02:38 +0800

    Netty版本Oio

---
+++ netty学习/Netty实战/chapter4/NettyOioServer.java
@@ -0,0 +1,54 @@
+package com.netty.NettyInAction.chapter4;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.oio.OioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.oio.OioServerSocketChannel;
+
+import java.net.InetSocketAddress;
+import java.nio.charset.Charset;
+
+/**
+ * @ Create by ostreamBaba on 18-7-27
+ * @ √Ë ˆ
+ */
+public class NettyOioServer {
+
+    public void server(int port) throws InterruptedException {
+        final ByteBuf buf= Unpooled.unreleasableBuffer(
+                Unpooled.copiedBuffer("Hi!\r\n",
+                        Charset.forName("UTF-8"))
+        );
+        EventLoopGroup group=new OioEventLoopGroup();
+        try {
+            ServerBootstrap bootstrap=new ServerBootstrap();
+            bootstrap.group(group)
+                    .channel( OioServerSocketChannel.class)
+                    .localAddress(new InetSocketAddress(port))
+                    .childHandler( new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline()
+                                    .addLast(new ChannelInboundHandlerAdapter(){
+                                        @Override
+                                        public void channelActive(ChannelHandlerContext ctx) throws Exception {
+                                            ctx.writeAndFlush(buf.duplicate())
+                                                    .addListener( ChannelFutureListener.CLOSE);
+                                        }
+                                    });
+                        }
+                    } );
+            ChannelFuture future=bootstrap.bind().sync();
+            future.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }finally {
+            group.shutdownGracefully().sync();
+        }
+    }
+
+}
